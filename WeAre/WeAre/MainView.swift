8c045a3e49b81c93e898e8d8bd5fa9d0229cde79
Nader Alfares <nna5040.psu@gmail.com>
2024-08-28 16:11:13 -0400

    ready for class

---
+++ WeAre/WeAre/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeAre
+//
+//  Created by Nader Alfares on 8/28/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack{
+            Text("mascot image")
+            Text("Cheer Text")
+            Button("Cheer Button") {}
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ WeAre/WeAre/WeAreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeAreApp.swift
+//  WeAre
+//
+//  Created by Nader Alfares on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WeAreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
