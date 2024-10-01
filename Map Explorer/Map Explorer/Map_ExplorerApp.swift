063bc52784e72c8166ef4cb0230c2ad648c576d0
Nader Alfares <nna5040.psu@gmail.com>
2024-10-01 12:06:19 -0400

    map explorer update

--- Map Explorer/Map Explorer/Map_ExplorerApp.swift
+++ Map Explorer/Map Explorer/Map_ExplorerApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct Map_ExplorerApp: App {
+    @State var manager = Manager()
     var body: some Scene {
         WindowGroup {
             MainView()
+                .environment(manager)
         }
     }
 }
