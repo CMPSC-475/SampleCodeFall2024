c66296e307e190665a180114068a28941bc749d0
Nader Alfares <nna5040.psu@gmail.com>
2024-10-15 10:54:28 -0400

    added activity indicator init proj

---
+++ ActivityIndicator/ActivityIndicator/ActivityIndicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityIndicatorApp.swift
+//  ActivityIndicator
+//
+//  Created by Nader Alfares on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ActivityIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
