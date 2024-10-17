32c93e2898142d1b31ca0151d5f38318e433cfb5
Nader Alfares <nna5040.psu@gmail.com>
2024-10-17 09:36:05 -0400

    ready for class

--- Map Explorer/Map Explorer/Model/CircleRegion.swift
+++ Map Explorer/Map Explorer/Model/CircleRegion.swift
@@ -5,6 +5,7 @@
 //  Created by Nader Alfares on 10/8/24.
 //
 import Foundation
+import MapKit
 
 struct CircleRegion : Identifiable{
     let center: Coord
@@ -13,5 +14,21 @@ struct CircleRegion : Identifiable{
     
     mutating func updateRadius(to newRadius:Double) {
         radius = newRadius
+    }
+}
+
+//for distinguishing between current circle region and existing regions (from dragging)
+enum RegionType : String, CaseIterable {
+    case current, done
+}
+extension CircleRegion {
+    
+    // make use of overlay title property to distinguish type of mkCircle (raw value of Region type is used)
+    func overlayWithTitle(_ title:String) -> MKCircle {
+        let center = CLLocationCoordinate2D(coord: self.center)
+        let radius = self.radius
+        let overlay = MKCircle(center: center, radius: radius)
+        overlay.title = title
+        return overlay
     }
 }
